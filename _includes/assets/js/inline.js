af34ba36a5f7292e163e3da740f1abdfd6ddecea
rocktronica <tommy@iamnotagoodartist.com>
2020-02-23 21:48:01 -0800

    Make it not seem totally open to the public

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -9,6 +9,48 @@ if (window.netlifyIdentity) {
 }
 
 (() => {
+    // Just trying to make it not seem totally open to the public..
+    const PASSWORD = "rsvp";
+
+    const modal = document.getElementById("password_modal");
+    const form = document.getElementById("password_form");
+    const input = document.getElementById("password");
+
+    const main = document.getElementById("main");
+
+    const enter = () => {
+        modal.style.display = "none";
+        main.style.display = "block";
+    }
+
+    if (
+        localStorage.password === PASSWORD &&
+        location.search !== `?${PASSWORD}`
+    ) {
+        enter();
+        return;
+    }
+
+    input.addEventListener("keydown", event => {
+        modal.classList.remove("invalid");
+    });
+
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+
+        const value = input.value.trim().toLowerCase();
+
+        localStorage.password = value;
+
+        if (value === PASSWORD) {
+            enter();
+        } else {
+            modal.classList.add("invalid");
+        }
+    });
+})();
+
+(() => {
     const attendeeSection = document.getElementById("show_only_if_attending");
     const showAttendeeSection = isAttending =>
         attendeeSection.style.display = isAttending ? null : "none";
@@ -17,7 +59,7 @@ if (window.netlifyIdentity) {
 
     Array.from(document.querySelectorAll(".attending_input")).forEach(input => {
       input.addEventListener("change", event => {
-          showAttendeeSection(event.target.value === "yes")
+          showAttendeeSection(event.target.value === "yes");
       });
     });
-})()
+})();
